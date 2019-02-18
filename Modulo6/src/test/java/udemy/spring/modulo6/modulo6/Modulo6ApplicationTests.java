780ba6619e8859ab0798b2e6ecd2c0b77d8e19fa
Bruno Schettini <brunovschettini@outlook.com>
2019-02-18 10:43:15 -0300

    =Aula que ensina apenas a criar links para páginas

---
+++ Modulo6/src/test/java/udemy/spring/modulo6/modulo6/Modulo6ApplicationTests.java
@@ -0,0 +1,16 @@
+package udemy.spring.modulo6.modulo6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Modulo6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
