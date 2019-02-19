67001059b916f3283e49a2b66ce0541c804507ee
Bruno Schettini <brunovschettini@outlook.com>
2019-02-19 18:11:03 -0300

    =Iniciando o módulo 8 com IntelliJ

---
+++ aulas/modulo8/src/main/java/com/aula/modulo8/modulo8/Modulo8Application.java
@@ -0,0 +1,13 @@
+package com.aula.modulo8.modulo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo8Application.class, args);
+	}
+
+}
