17a82945abdb5f3ca314041ce0ab466ed20b283f
Bruno Schettini <brunovschettini@outlook.com>
2019-02-26 18:11:27 -0300

    =Pŕoximo módulo

---
+++ Certificacao/src/certificacao/Certificacao.java
@@ -0,0 +1,36 @@
+package certificacao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author rtools2
+ */
+public class Certificacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        List oList = new ArrayList();
+//        for (int i = 0; i < 10; i++) {
+//            Object[] o = new Object[]{"A", "B", "C"};
+//            oList.add(o);
+//        }
+//        for (int i = 0; i < oList.size(); i++) {
+//            Object[] o2 = (Object[]) oList.get(i);
+//            for (Object o21 : o2) {
+//                System.out.println((i + 1) + " - " + o21);
+//            }
+//        }
+//
+//        List list = Arrays.asList(oList);
+//        System.out.println(list);
+
+        System.gc();
+
+    }
+
+}
--- src/main/java/natan/io/projeto1/controllers/UserController.java
+++ src/main/java/natan/io/projeto1/controllers/UserController.java
@@ -51,4 +51,4 @@ public class UserController {
         return "user";
     }
 
-}
+    }
